4616c22abcdff9baa19eb0110816adfc64bd0a8b
guillermo <guillermouser.iti@hotmail.com>
2019-01-30 09:38:31 -0600

    memorroidez

---
+++ Practipopas/Practipopas/memo.swift
@@ -0,0 +1,13 @@
+//
+//  memo.swift
+//  Practipopas
+//
+//  Created by Universidad Politecnica de Gómez Palacio on 30/01/19.
+//  Copyright © 2019 Universidad Politecnica de Gómez Palacio. All rights reserved.
+//
+
+import UIKit
+
+class memo: NSObject {
+
+}
